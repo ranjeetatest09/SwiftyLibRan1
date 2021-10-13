a2c73aa7d1dd7e1f2bd4975f195e4299603d28f7
Ranjeeta Rajput <ranjeeta.rajput@cerence.com>
2021-10-13 17:49:18 +0530

    initial commit

---
+++ SwiftyLibRan1/SwiftyLibRan1.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibRan1.h
+//  SwiftyLibRan1
+//
+//  Created by Ranjeeta Rajput on 13/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibRan1.
+FOUNDATION_EXPORT double SwiftyLibRan1VersionNumber;
+
+//! Project version string for SwiftyLibRan1.
+FOUNDATION_EXPORT const unsigned char SwiftyLibRan1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibRan1/PublicHeader.h>
+
+

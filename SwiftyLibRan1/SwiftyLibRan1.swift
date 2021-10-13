c53ce70ee694a8de608b92c6d6ff1dffad8e0f9b
Ranjeeta Rajput <ranjeeta.rajput@cerence.com>
2021-10-13 18:02:29 +0530

    1

---
+++ SwiftyLibRan1/SwiftyLibRan1.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLibRan1.swift
+//  SwiftyLibRan1
+//
+//  Created by Ranjeeta Rajput on 13/10/21.
+//
+
+//SwiftyLibRan1
+
+public class SwiftyLibRan1 {
+
+    let name = "SwiftyLibRan1"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLibRan1Tests/SwiftyLibRan1Tests.swift
+++ SwiftyLibRan1Tests/SwiftyLibRan1Tests.swift
@@ -9,25 +9,38 @@ import XCTest
 @testable import SwiftyLibRan1
 
 class SwiftyLibRan1Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    var swiftyLibran: SwiftyLibRan1!
+//
+//    override class func setUp() {
+//        swiftyLibran = SwiftyLibRan1()
+//    }
+//    
+//    func testAdd() {
+//          XCTAssertEqual(swiftyLibran.add(a: 1, b: 1), 2)
+//      }
+//      
+//      func testSub() {
+//          XCTAssertEqual(swiftyLibran.sub(a: 2, b: 1), 1)
+//      }
+    
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
